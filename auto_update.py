11d2aff8e9240d4e7eb545ed0c14ef2ee3b3ebc1
dravendraven <vitorolg@gmail.com>
2026-03-19 23:15:52 -0300

    v9.1

--- auto_update.py
+++ auto_update.py
@@ -12,7 +12,7 @@ from tkinter import ttk
 
 # ================= CONFIGURAÇÕES =================
 # Versão atual hardcoded (atualizada automaticamente pelo publicar.bat)
-CURRENT_VERSION = "8.9"
+CURRENT_VERSION = "9.0"
 
 # URLs do GitHub
 URL_VERSION = "https://raw.githubusercontent.com/dravendraven/molodoybot/refs/heads/main/version.txt"
